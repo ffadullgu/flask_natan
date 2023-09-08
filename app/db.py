7bb0159b00afc25dc6f42318b369af3f2d3553c8
Fabian Fadul <ffadullgu@yahoo.com>
2023-09-08 08:44:51 -0500

    sextod

--- app/db.py
+++ app/db.py
@@ -11,7 +11,7 @@ app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or 'root'
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or 'L51sJWZ2fftDHwo48o9j'
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or 'containers-us-west-144.railway.app' # localhost
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') or 'railway'
-app.config['MYSQL_PORT'] = os.getenv('MYSQL_PORT') or '5876'
+app.config['MYSQL_PORT'] = os.getenv('MYSQL_PORT') or 5876
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # MySQL Connection
 mysql = MySQL(app)
