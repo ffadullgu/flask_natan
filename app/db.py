16ee0de40fa613bee67b4558c382d198f373fa2a
Fabian Fadul <ffadullgu@yahoo.com>
2023-09-08 07:01:59 -0500

    tercero

---
+++ app/db.py
@@ -0,0 +1,16 @@
+from app import app
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # take environment variables from .env.
+
+# Mysql Settings
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or 'root'
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or 'L51sJWZ2fftDHwo48o9j'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or 'containers-us-west-144.railway.app' # localhost
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') or 'flaskcrud'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# MySQL Connection
+mysql = MySQL(app)
